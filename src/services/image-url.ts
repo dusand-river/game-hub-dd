9a0900b8d301fcaad3b31af0e66398920a8fde39
Dusan <dusandavidovic@gmail.com>
2023-04-03 15:49:48 -0400

    games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,10 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
   // transfer incoming url to cropped one
   // old: ....rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
   // new: ....rawg.io/media/crop/600/400/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
